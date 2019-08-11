a67355ef62b7dde5791bc459bdd3870e10bfb656
Fenil Mehta <fenilgmehta@gmail.com>
2019-08-11 12:07:30 +0530

    Update compile.sh to handle incorrect number of command line parameters

--- test/compile.sh
+++ test/compile.sh
@@ -1,16 +1,24 @@
 # /usr/bin/time -f "%M KB used" ./a.out < 3_input
+# $1: name of the source file to be compiled
+# $2: (optional) output object file name
 
-if [[ $# -eq 1 ]]
+if [[ $# < 1 || $# > 2 ]]
 then
-    # if [[ "a.out" -ot "${1}" || "a.out" -nt "${1}" ]]
-    # then
-        echo "Compiling ..." > "/dev/stderr"
-        g++ -O2 "${1}"
-    # fi;
+    echo "Usage: compile.sh [source_file.cpp] [a.out]" > "/dev/stderr"
 else
-    # if [[ "$2" -ot "${1}" || "$2" -nt "${1}" ]]
-    # then
-        echo "Compiling ..." > "/dev/stderr"
-        g++ -O2 "${1}" -o "${2}"
-    # fi;
+    if [[ $# -eq 1 ]]
+    then
+        # if [[ "a.out" -ot "${1}" || "a.out" -nt "${1}" ]]
+        # then
+            echo "Compiling ..." > "/dev/stderr"
+            g++ -O2 "${1}"
+        # fi;
+    else
+        # if [[ "$2" -ot "${1}" || "$2" -nt "${1}" ]]
+        # then
+            echo "Compiling ..." > "/dev/stderr"
+            g++ -O2 "${1}" -o "${2}"
+        # fi;
+    fi;
 fi;
+
